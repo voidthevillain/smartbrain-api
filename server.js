195673047ee2cdf4bf77919c9027b79ce3bde422
Mihai Filip <voidthevillain@users.noreply.github.com>
2019-11-14 17:11:25 +0100

    add connectionString

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL, // localhost
+    connectionString: process.env.DATABASE_URL, // localhost
     ssl: true
   }
 });
