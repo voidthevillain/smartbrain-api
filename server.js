3ba5bbbdd13671f3ceed9857f9d97133feb4cfb4
Mihai Filip <voidthevillain@users.noreply.github.com>
2019-11-14 17:00:41 +0100

    change db host from local to heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1", // localhost
+    host: "postgresql-curved-27094", // localhost
     user: "postgres",
     password: "hesoyam",
     database: "smartbrain"
