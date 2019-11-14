8bb54abe0570c35bbad21e6f4026fa94f9e49da0
Mihai Filip <voidthevillain@users.noreply.github.com>
2019-11-14 16:11:09 +0100

    complete back-end

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "680cd1785495494b98434f497aa5af2e"
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input).then(data => {
+    res.json(data)
+  }).catch(err => res.status(400).json('Unable to work with API.'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Unable to get entries."));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
